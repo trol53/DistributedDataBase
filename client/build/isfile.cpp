3d9008ce1033fb8fb3033892d9ee57989ba1bb4f
trol53 <nek.savrik@yandex.ru>
2025-04-28 17:15:37 +0300

    remove build files

--- client/build/isfile.cpp
+++
@@ -1,9 +0,0 @@
-#include <filesystem>
-#include <string>
-#include <iostream>
-
-int main(){
-	std::string file("/home/trol53/pets/DistributedDataBase/client/build/test1");
-	std::filesystem::path p(file);
-	std::cout << std::filesystem::exists(p);
-}
